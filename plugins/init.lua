fa227798b42911d719a2f1b2808d8117c0bef660
Tiago Carioca <tiagoacarioca@gmail.com>
2023-04-01 17:39:39 -0400

    :tada: first commit :tada:

---
+++ plugins/init.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "Exafunction/codeium.vim"
+  },
+  {
+    "tpope/vim-fugitive"
+  },
+  {
+    "karb94/neoscroll.nvim",
+    init = function() table.insert(astronvim.file_plugins, "neoscroll.nvim") end,
+    opts = {
+      -- All these keys will be mapped to their corresponding default scrolling animation
+      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      hide_cursor = true,          -- Hide cursor while scrolling
+      stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+      use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+      respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      easing_function = nil,       -- Default easing function
+      pre_hook = nil,              -- Function to run before the scrolling animation starts
+      post_hook = nil,             -- Function to run after the scrolling animation ends
+    },
+  },
+  { "lvimuser/lsp-inlayhints.nvim", config = true },
+  { "nvim-treesitter/playground",   cmd = "TSHighlightCapturesUnderCursor", dependencies = { "nvim-treesitter" } },
+  {
+    "nvim-telescope/telescope.nvim",
+    defaults = {
+      file_ignore_patterns = {
+        "node_modules"
+      }
+    }
+  }
+}
---
+++ plugins/mason-lspconfig.lua
@@ -0,0 +1,18 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  opts = {
+    automatic_installation = true,
+    ensure_installed = {
+      "clangd",
+      "cssls",
+      "html",
+      "marksman",
+      "jsonls",
+      "pyright",
+      "sqls",
+      "sumneko_lua",
+      "tsserver",
+      "yamlls",
+    },
+  },
+}
