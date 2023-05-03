e193278cbfa3a37282d8dbc8d2358e925c8c3e63
Tiago Carioca <tiagoacarioca@gmail.com>
2023-05-03 17:06:40 -0400

    feat: add community plugins

--- autocmds.lua
+++ autocmds.lua
@@ -5,18 +5,4 @@ vim.api.nvim_create_autocmd(
 
 require("telescope").setup { defaults = { file_ignore_patterns = { "node_modules" } } }
 
--- require("neo-tree").setup {
---   filesystem = {
---     filtered_items = {
---       hide_dotfiles = false,
---       hide_gitignored = false,
---       hide_by_name = {
---         ".DS_Store",
---         "node_modules",
---         ".git",
---       },
---     },
---   },
--- }
-
 require("todo-comments").setup()
---
+++ plugins/community.lua
@@ -0,0 +1,5 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.colorscheme.tokyonight", enable = true },
+}
